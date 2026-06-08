3af5218bf74786a5ade508fcdcf730b81405076c
srikanth kuruma <srikshayla@gmail.com>
2026-06-08 17:14:43 +0000

    yes commit final

--- models/emp_08.sql
+++
@@ -1,9 +0,0 @@
-select current_database(),current_schema()
-
-select * from SAMPLE1.DBT_SRIKSHAYLACODE.ORDERS
-
-
-select '{{target.database}}','{{target.schema}}','{{target.type}}'
-
-select
-    '{{ invocation_id }}' as invocation_id
